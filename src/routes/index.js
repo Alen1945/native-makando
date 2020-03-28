eb9a96202dabf7f3e80cf4d5fc1b4f129f389b45
Alen1945 <dwialendraipangselly23@gmail.com>
2020-03-28 09:25:27 +0700

    check token expired

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import {actionLogout} from '../store/actions/actionsUserData';
+import jwt_decode from 'jwt-decode';
+
 function Routes(props) {
-  const isLogin = useSelector(state => state.dataUser.isLogin);
-  if (isLogin) {
+  const dispatch = useDispatch();
+  const {isLogin, token} = useSelector(state => state.dataUser);
+  if (isLogin && token) {
+    const payload = jwt_decode(token);
+    if (new Date(payload.exp * 1000).getTime() - new Date().getTime() <= 0) {
+      return dispatch(actionLogout());
+    }
     return <PrivateRoute />;
   } else {
     return <PublicRoute />;

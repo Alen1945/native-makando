b6824958046d1c8881acb699711fd464f9ed2923
Alen1945 <dwialendraipangselly23@gmail.com>
2020-03-29 16:59:21 +0700

    update ui list item

--- src/screens/Items/index.js
+++ src/screens/Items/index.js
@@ -6,8 +6,12 @@ import {
   TouchableHighlight,
   ImageBackground,
 } from 'react-native';
-import {ScrollView, FlatList} from 'react-native-gesture-handler';
-import {SearchBar, Button} from 'react-native-elements';
+import {
+  ScrollView,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native-gesture-handler';
+import {SearchBar, Button, Card, Avatar} from 'react-native-elements';
 import getData from '../../helpers/getData';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import {API_URL} from 'react-native-dotenv';
@@ -15,6 +19,7 @@ import formatRupiah from '../../helpers/formatRupiah';
 function Items(props) {
   const {navigation} = props;
   const [activeCategory, setActiveCategory] = React.useState(0);
+  const [activePage, setActivePage] = React.useState(1);
   const [categories, setCategories] = React.useState([]);
   const [items, setItems] = React.useState([]);
   const [search, setSearch] = React.useState('');
@@ -30,7 +35,7 @@ function Items(props) {
   };
   const getItems = async (page, category) => {
     try {
-      const condition = `limit=5&sort[created_at]=1&page=${page}`;
+      const condition = `limit=20&sort[created_at]=1&page=${page}`;
       let url = `/browse-items?${condition}`;
       if (category) {
         url = `/browse-categories/${category}?${condition}`;
@@ -53,166 +58,256 @@ function Items(props) {
     getCategories();
   }, []);
   React.useEffect(() => {
-    getItems(1, activeCategory);
-  }, [activeCategory]);
+    getItems(activePage, activeCategory);
+  }, [activePage, activeCategory]);
   React.useEffect(() => {
     if (search && search.trim() && search.length > 3) {
-      getItems(1, activeCategory);
+      getItems(activePage, activeCategory);
     } else if (search.length === 0) {
-      getItems(1, activeCategory);
+      getItems(activePage, activeCategory);
     }
-  }, [search]);
+  }, [search, activePage]);
   return (
-    <View
-      style={{flex: 1, backgroundColor: '#fff', padding: 10, paddingTop: 20}}>
-      <View style={{paddingHorizontal: 10}}>
-        <View style={{paddingHorizontal: 10, marginBottom: 10}}>
-          <SearchBar
-            placeholder="Search Here..."
-            round={false}
-            value={search}
-            onChangeText={value => setSearch(value)}
-            searchIcon={<Icon name="search" size={15} color="#999" />}
-            containerStyle={{
-              backgroundColor: 'rgba(0,0,0,0)',
-              padding: 0,
-              borderBottomWidth: 1,
-              borderBottomColor: '#999',
-              borderTopWidth: 0,
-              marginBottom: 10,
-            }}
-            inputContainerStyle={{
-              backgroundColor: 'rgba(255,255,255,.5)',
-              borderRadius: 25,
-              paddingHorizontal: 10,
-            }}
-            inputStyle={{
-              backgroundColor: 'transparent',
-            }}
+    <>
+      <View
+        style={{
+          elevation: 4,
+          backgroundColor: '#FFF',
+          height: 50,
+          paddingHorizontal: 10,
+          justifyContent: 'center',
+        }}>
+        <Text
+          style={{
+            fontSize: 17,
+            fontWeight: 'bold',
+            textAlign: 'center',
+            color: '#555',
+          }}>
+          Avaible Items
+        </Text>
+      </View>
+      <View style={{flex: 1, backgroundColor: '#fff', padding: 10}}>
+        <View style={{paddingHorizontal: 10}}>
+          <View style={{paddingHorizontal: 10, marginBottom: 10}}>
+            <SearchBar
+              placeholder="Search Here..."
+              round={false}
+              value={search}
+              onChangeText={value => setSearch(value)}
+              searchIcon={<Icon name="search" size={15} color="#999" />}
+              containerStyle={{
+                backgroundColor: 'rgba(0,0,0,0)',
+                padding: 0,
+                borderBottomWidth: 1,
+                borderBottomColor: '#999',
+                borderTopWidth: 0,
+                marginBottom: 10,
+              }}
+              inputContainerStyle={{
+                backgroundColor: 'rgba(255,255,255,.5)',
+                borderRadius: 25,
+                paddingHorizontal: 10,
+              }}
+              inputStyle={{
+                backgroundColor: 'transparent',
+              }}
+            />
+          </View>
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+            <View
+              style={{
+                height: 60,
+                flexDirection: 'row',
+                alignItems: 'flex-start',
+                paddingVertical: 10,
+              }}>
+              <View style={{width: 80, marginHorizontal: 3}}>
+                <Button
+                  title="Show All"
+                  onPress={() => handleChangeCategory(0)}
+                  type={parseInt(activeCategory) === 0 ? 'solid' : 'outline'}
+                  containerStyle={{width: '100%'}}
+                  buttonStyle={{
+                    borderColor: `${
+                      parseInt(activeCategory) === 0 ? '#fff' : '#f54251'
+                    }`,
+                    backgroundColor: `${
+                      parseInt(activeCategory) === 0 ? '#f54251' : '#fff'
+                    }`,
+                  }}
+                  titleStyle={{
+                    fontSize: 12,
+                    color: `${
+                      parseInt(activeCategory) == 0 ? '#fff' : '#f54251'
+                    }`,
+                  }}
+                />
+              </View>
+              {categories &&
+                categories.map(category => (
+                  <View
+                    style={{width: 80, marginHorizontal: 3}}
+                    key={category._id}>
+                    <Button
+                      onPress={() => handleChangeCategory(category._id)}
+                      title={category.name}
+                      type={
+                        parseInt(activeCategory) === category._id
+                          ? 'solid'
+                          : 'outline'
+                      }
+                      containerStyle={{width: '100%'}}
+                      buttonStyle={{
+                        borderColor: `${
+                          parseInt(activeCategory) === category._id
+                            ? '#fff'
+                            : '#f54251'
+                        }`,
+                        backgroundColor: `${
+                          parseInt(activeCategory) === category._id
+                            ? '#f54251'
+                            : '#fff'
+                        }`,
+                      }}
+                      titleStyle={{
+                        fontSize: 12,
+                        color: `${
+                          parseInt(activeCategory) == category._id
+                            ? '#fff'
+                            : '#f54251'
+                        }`,
+                      }}
+                    />
+                  </View>
+                ))}
+            </View>
+          </ScrollView>
+        </View>
+        <View style={{marginBottom: 100}}>
+          <FlatList
+            data={items.dataItems}
+            keyExtractor={item => item.name + item._id}
+            showsVerticalScrollIndicator={false}
+            numColumns={2}
+            contentContainerStyle={{alignItems: 'center', paddingBottom: 60}}
+            renderItem={({item}) => (
+              <Card
+                containerStyle={{
+                  width: 160,
+                  borderWidth: 0,
+                  marginHorizontal: 5,
+                  alignItems: 'center',
+                }}>
+                <TouchableOpacity
+                  onPress={() =>
+                    navigation.navigate('DetailScreens', {
+                      screen: 'DetailItem',
+                      params: {
+                        id: item._id,
+                      },
+                    })
+                  }>
+                  <>
+                    <Avatar
+                      rounded
+                      title={item.name.substring(0, 2).toUpperCase()}
+                      size={110}
+                      containerStyle={{
+                        marginBottom: 5,
+                      }}
+                      source={{uri: `${API_URL}/${item.images}`}}
+                    />
+                    <Text
+                      style={{
+                        textAlign: 'center',
+                        fontWeight: 'bold',
+                        textTransform: 'capitalize',
+                        fontSize: 14,
+                      }}>
+                      {item.name}
+                    </Text>
+                  </>
+                </TouchableOpacity>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'flex-start',
+                    alignItems: 'center',
+                  }}>
+                  <Icon
+                    name="location-arrow"
+                    size={10}
+                    style={{marginRight: 10, color: '#d15a64'}}
+                  />
+                  <Text style={{fontSize: 12, color: '#666'}}>
+                    {item.name_category}
+                  </Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'flex-start',
+                    alignItems: 'center',
+                  }}>
+                  <Icon
+                    name="store-alt"
+                    size={10}
+                    style={{marginRight: 10, color: '#d15a64'}}
+                  />
+                  <Text style={{fontSize: 10, color: '#666'}}>
+                    {item.name_restaurant}
+                  </Text>
+                </View>
+                <Text
+                  style={{
+                    backgroundColor: '#f54251',
+                    padding: 3,
+                    marginVertical: 5,
+                    borderRadius: 10,
+                    textAlign: 'center',
+                    fontSize: 15,
+                    color: '#FFF',
+                    fontWeight: 'bold',
+                  }}>
+                  Rp. {formatRupiah(item.price)}
+                </Text>
+              </Card>
+            )}
           />
         </View>
-        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+        {/* {true && (
           <View
             style={{
-              height: 60,
-              flexDirection: 'row',
-              alignItems: 'flex-start',
-              paddingVertical: 10,
+              paddingTop: 20,
+              paddingHorizontal: 10,
+              marginBottom: 100,
+              alignItems: 'center',
+              justifyContent: 'center',
             }}>
-            <View style={{width: 80, marginHorizontal: 3}}>
-              <Button
-                title="Show All"
-                onPress={() => handleChangeCategory(0)}
-                type={parseInt(activeCategory) === 0 ? 'solid' : 'outline'}
-                containerStyle={{width: '100%'}}
-                buttonStyle={{
-                  borderColor: `${
-                    parseInt(activeCategory) === 0 ? '#fff' : '#f54251'
-                  }`,
-                  backgroundColor: `${
-                    parseInt(activeCategory) === 0 ? '#f54251' : '#fff'
-                  }`,
-                }}
-                titleStyle={{
-                  fontSize: 12,
-                  color: `${
-                    parseInt(activeCategory) == 0 ? '#fff' : '#f54251'
-                  }`,
-                }}
-              />
-            </View>
-            {categories &&
-              categories.map(category => (
-                <View
-                  style={{width: 80, marginHorizontal: 3}}
-                  key={category._id}>
-                  <Button
-                    onPress={() => handleChangeCategory(category._id)}
-                    title={category.name}
-                    type={
-                      parseInt(activeCategory) === category._id
-                        ? 'solid'
-                        : 'outline'
-                    }
-                    containerStyle={{width: '100%'}}
-                    buttonStyle={{
-                      borderColor: `${
-                        parseInt(activeCategory) === category._id
-                          ? '#fff'
-                          : '#f54251'
-                      }`,
-                      backgroundColor: `${
-                        parseInt(activeCategory) === category._id
-                          ? '#f54251'
-                          : '#fff'
-                      }`,
-                    }}
-                    titleStyle={{
-                      fontSize: 12,
-                      color: `${
-                        parseInt(activeCategory) == category._id
-                          ? '#fff'
-                          : '#f54251'
-                      }`,
-                    }}
-                  />
-                </View>
-              ))}
+            <Text style={{marginBottom: 20, color: '#444', fontSize: 20}}>
+              ini Page
+            </Text>
           </View>
-        </ScrollView>
+        )} */}
+        {(!items.dataItems || !items.dataItems.length) && (
+          <View
+            style={{
+              paddingTop: 20,
+              paddingHorizontal: 10,
+              marginTop: -90,
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Text style={{marginBottom: 20, color: '#444', fontSize: 20}}>
+              Data Empty
+            </Text>
+            <Image source={require('../../../icons/emptyItems.png')} />
+          </View>
+        )}
       </View>
-      <View style={{marginBottom: 100, paddingHorizontal: 5}}>
-        <FlatList
-          data={items.dataItems}
-          keyExtractor={item => item.name + item._id}
-          showsVerticalScrollIndicator={false}
-          renderItem={({item}) => (
-            <View style={{marginBottom: 40}}>
-              <TouchableHighlight
-                onPress={() =>
-                  navigation.navigate('DetailScreens', {
-                    screen: 'DetailItem',
-                    params: {
-                      id: item._id,
-                    },
-                  })
-                }>
-                <>
-                  <ImageBackground
-                    style={{height: 200, width: '100%', marginBottom: 10}}
-                    source={{uri: `${API_URL}/${item.images}`}}
-                  />
-                  <Text
-                    style={{
-                      fontWeight: 'bold',
-                      textTransform: 'capitalize',
-                      fontSize: 18,
-                    }}>
-                    {item.name}
-                  </Text>
-                </>
-              </TouchableHighlight>
-            </View>
-          )}
-        />
-      </View>
-      {(!items.dataItems || !items.dataItems.length) && (
-        <View
-          style={{
-            paddingTop: 20,
-            paddingHorizontal: 10,
-            marginTop: -90,
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          <Text style={{marginBottom: 20, color: '#444', fontSize: 20}}>
-            Data Empty
-          </Text>
-          <Image source={require('../../../icons/emptyItems.png')} />
-        </View>
-      )}
-    </View>
+    </>
   );
 }
 
